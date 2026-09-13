9c8bacb171403e3e796e7d9febaaa820db60fd30
citizenready26 <draj302002@gmail.com>
2026-09-13 18:46:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 /* Public Supabase settings only. Never place a service_role key in this file. */
-window.THE_INDEX_SUPABASE_URL = window.https://abqlweiiwhjjpxdlokar.supabase.co/rest/v1/|| '';
-window.THE_INDEX_SUPABASE_ANON_KEY = window.sb_publishable_ogedpT56tgb0X2LUaHAAzg_P7Rg3mws || '';
+window.THE_INDEX_SUPABASE_URL = 'https://abqlweiiwhjjpxdlokar.supabase.co ';
+window.THE_INDEX_SUPABASE_ANON_KEY = 'sb_publishable_ogedpT56tgb0X2LUaHAAzg_P7Rg3mws';
