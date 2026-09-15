c80ca55a44698d11e2f8605d619d7669376fd5af
citizenready26 <draj302002@gmail.com>
2026-09-15 06:45:09 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-window.INDEX_CONFIG = {
-  supabaseUrl: 'https://abqlweiiwhjjpxdlokar.supabase.co',
-  supabaseKey: 'sb_publishable_ogedpT56tgb0X2LUaHAAzg_P7Rg3mws',
-  storageBucket: 'service-images'
-};
